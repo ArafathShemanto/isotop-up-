5d9552354bddb0f76ac43e38265f138fc1410aaa
MD ARAFATH HOSSAIN SHEMANTO <48240273+mdarafath03@users.noreply.github.com>
2019-04-30 11:25:22 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+
+    
+    
+// init Isotope
+var $grid = $('.grid').isotope({
+  itemSelector: '.grid-item',
+  percentPosition: true,
+  masonry: {
+    // use outer width of grid-sizer for columnWidth
+    columnWidth: 1
+  }
+})
+// filter items on button click
+$('.portfolio-menu').on( 'click', 'button', function() {
+  var filterValue = $(this).attr('data-filter');
+  $grid.isotope({ filter: filterValue });
+});
+
+	//for menu active class
+	$('.portfolio-menu button').on('click', function(event) {
+		$(this).siblings('.active').removeClass('active');
+		$(this).addClass('active');
+		event.preventDefault();
+	});
+
+
+    
+ 
+    
+    
+    
+});
